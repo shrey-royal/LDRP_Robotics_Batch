4196066bf6dc1ff1b2d02c0c4ade4708a83d5dbe
Shrey K <srkkadia@gmail.com>
2025-06-08 11:52:15 +0530

    IR

---
+++ Level 2/08_IR/01_Protocol_Finder/01_Protocol_Finder.ino
@@ -0,0 +1,15 @@
+#include "IRremote.hpp"
+
+#define IR_RECEIVE_PIN 2
+
+void setup() {
+  Serial.begin(115200);
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+}
+
+void loop() {
+  if (IrReceiver.decode()) {
+    IrReceiver.printIRResultShort(&Serial);
+    IrReceiver.resume();
+  }
+}
